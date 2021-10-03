0b5edc2e6e1ee2ea0752ca89dea5eb65784df791
Max Makarov <makarov@mail.ru>
2021-10-03 08:36:49 +0300

    Add EntityFrameworkCore, EntityFrameworkCore.Jet

---
+++ ContactBook/Models/Contact.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBook.Models
+{
+    public class Contact
+    {
+        /// <summary>
+        /// Идентификатор контакта
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string Surname { get; set; }
+        /// <summary>
+        /// Дата рождения
+        /// </summary>
+        public DateTime Birthday { get; set; }
+        /// <summary>
+        /// Комания
+        /// </summary>
+        public string Company { get; set; }
+        /// <summary>
+        /// E-Mail
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Телефон +7 (***) ***-**-**
+        /// </summary>        
+        public string Phone { get; set; }
+        /// <summary>
+        /// Избранный контакт
+        /// </summary>
+        public bool Favorite { get; set; }
+    }
+}
---
+++ ContactBook/Models/ContactContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBook.Models
+{
+    public class ContactContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            //The issue occurrs here
+
+            optionsBuilder.UseJet(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=c:\Users\MrBoss\source\repos\Contacts\Contacts\bin\Debug\Data.accdb;");
+
+        }
+    }
+}
